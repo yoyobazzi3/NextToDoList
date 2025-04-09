7734898a937c85e7d6200769c1291e58ea5ca6fc
yoyobazzi3 <yoyobazzi3@gmail.com>
2025-04-09 13:45:53 -0700

    get backend

--- next-todolist/src/app/api/item/route.ts
+++ next-todolist/src/app/api/item/route.ts
@@ -4,22 +4,50 @@ import Item from "../../../../models/model";
 
 export async function POST(req: NextRequest) {
     try {
-      await connectToDatabase();
-  
-      const { title, description } = await req.json(); 
-  
-      const newItem = await Item.create({ title, description });
-  
-      return new Response(JSON.stringify(newItem), {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+        await connectToDatabase();
+
+        const { title, description } = await req.json();
+
+        const newItem = await Item.create({ title, description });
+
+        return new Response(JSON.stringify(newItem), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
     } catch (error) {
-      console.error("Server Error:", error);
-      return new Response(JSON.stringify({ message: "Internal server error" }), {
-        status: 500,
-      });
+        console.error("Server Error:", error);
+        return new Response(JSON.stringify({ message: "Internal server error" }), {
+            status: 500,
+        });
     }
-  }
+}
+export async function GET(req: NextRequest) {
+    try {
+        await connectToDatabase();
+        const items = await Item.find({}).sort({ createdAt: -1 });
+        if (!items || items.length === 0) {
+            return new Response(JSON.stringify({ message: "No items found" }), {
+                status: 404,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+        return new Response(JSON.stringify(items),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+        })
+    }
+    catch (error) {
+        console.error("Server Error:", error);
+        return new Response(JSON.stringify({ message: "Internal server error" }), {
+            status: 500,
+        });
+    }
+}
